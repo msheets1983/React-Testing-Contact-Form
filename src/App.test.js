53b4506ea9b5431643d56cefc480de66fb529e7f
Michael Sheets <michaelsheets@MacBook-Pro.local>
2020-06-04 19:31:49 -0500

    MVP met

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,103 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent, act, screen } from "@testing-library/react";
 import App from "./App";
+import ContactForm from './components/ContactForm'
 
+//Test
 test("renders App without crashing", () => {
   render(<App />);
+});
+
+//Arrange
+test('form can be filled in', () => {
+  render(<ContactForm />)
+
+  //Act
+  const firstNameInput = screen.getByLabelText(/First Name/i)
+  const lastNameInput = screen.getByLabelText(/Last Name/i)
+  const emailInput = screen.getByLabelText(/email/i)
+  const messageInput = screen.getByLabelText(/message/i)
+
+  //Assert
+  expect(firstNameInput).toBeVisible();
+  expect(lastNameInput).toBeVisible();
+  expect(emailInput).toBeVisible();
+  expect(messageInput).toBeVisible();
+
+});
+
+test('Test values can be intered into inputs', () => {
+  //Arrange
+  render(<ContactForm />)
+
+  const firstNameInput = screen.getByLabelText(/First Name/i)
+  const lastNameInput = screen.getByLabelText(/Last Name/i)
+  const emailInput = screen.getByLabelText(/email/i)
+  const messageInput = screen.getByLabelText(/message/i)
+
+  //Act
+  fireEvent.change(firstNameInput, { target: { value: 'Michael' } })
+  fireEvent.change(lastNameInput, { target: { value: 'Sheets' } })
+  fireEvent.change(emailInput, { target: { value: 'michael.sheets83@gmail.com' } })
+  fireEvent.change(messageInput, { target: { value: 'This is a message' } })
+
+  //Assert
+  expect(firstNameInput.value).toBe('Michael')
+  expect(lastNameInput.value).toBe('Sheets')
+  expect(emailInput.value).toBe('michael.sheets83@gmail.com')
+  expect(messageInput.value).toBe('This is a message')
+
+});
+
+test('Test maxLength for firstName input is 10 char instead of 3', async () => {
+  //Arrange
+  render(<ContactForm />)
+  const firstNameInput = screen.getByLabelText(/First Name*/i)
+  const submitButton = screen.getByTestId(/Submit/i)
+  const firstNameError = screen.findByTestId(/firstNameError/i)
+
+  //Act
+  await act(() => {
+    fireEvent.change(firstNameInput, { target: { value: '12345689a' } });
+    fireEvent.click(submitButton);
+  });
+
+  //Assert
+  expect(firstNameError).toBeNull;
+});
+
+test('Form submit add new contact', async () => {
+  await act(async () => {
+
+    //Arrange
+    const { getByLabelText, getByTestId, findByTestId } = render(<ContactForm />);
+    const firstNameInput = getByLabelText(/First Name*/i);
+    const lastNameInput = getByLabelText(/Last Name/i);
+    const emailInput = getByLabelText(/email/i);
+    const messageInput = getByLabelText(/message/i);
+    const submitButton = getByTestId(/Submit/i);
+    const success = findByTestId(/success/i);
+
+    //Act
+    await act(() => {
+      fireEvent.change(firstNameInput, { target: { value: 'Michael' } })
+      fireEvent.change(lastNameInput, { target: { value: 'Sheets' } })
+      fireEvent.change(emailInput, { target: { value: 'michael.sheets83@gmail.com' } })
+      fireEvent.change(messageInput, { target: { value: 'This is a message' } })
+    })
+
+    //Assert
+    expect(firstNameInput.value).toBe('Michael')
+    expect(lastNameInput.value).toBe('Sheets')
+    expect(emailInput.value).toBe('michael.sheets83@gmail.com')
+    expect(messageInput.value).toBe('This is a message')
+
+    //Act
+    await act (() =>{
+      fireEvent.click(submitButton);
+    })
+
+    //Assert
+    expect(findByTestId(success)).not.toBeNull();
+  })
 });
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
+
 const ContactForm = () => {
   const [data, setData] = useState();
   const { register, errors, handleSubmit } = useForm({
@@ -9,25 +10,26 @@ const ContactForm = () => {
   const onSubmit = (data) => {
     setData(data);
   };
-
   return (
     <div className="App">
       <form onSubmit={handleSubmit(onSubmit)}>
         <div>
           <label htmlFor="firstName">First Name*</label>
           <input
+            id="firstName"
             name="firstName"
             placeholder="Edd"
-            ref={register({ required: true, maxLength: 3 })}
+            ref={register({ required: true, maxLength: 10 })}
           />
           {errors.firstName && (
-            <p>Looks like there was an error: {errors.firstName.type}</p>
+            <p data-testid="firstNameError">Looks like there was an error: {errors.firstName.type}</p>
           )}
         </div>
 
         <div>
           <label htmlFor="lastName">Last Name*</label>
           <input
+            id="lastName"
             name="lastName"
             placeholder="Burke"
             ref={register({ required: true })}
@@ -36,29 +38,27 @@ const ContactForm = () => {
             <p>Looks like there was an error: {errors.lastName.type}</p>
           )}
         </div>
-
         <div>
           <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
             Email*
           </label>
-          <input name="email" ref={register({ required: true })} />
+          <input id="email" name="email" ref={register({ required: true })} />
           {errors.email && (
             <p>Looks like there was an error: {errors.email.type}</p>
           )}
         </div>
         <div>
           <label htmlFor="message">Message</label>
-          <textarea name="message" ref={register({ required: false })} />
+          <textarea id='message' name="message" ref={register({ required: false })} />
         </div>
         {data && (
-          <pre style={{ textAlign: "left", color: "white" }}>
+          <pre data-testid='success' style={{ textAlign: "left", color: "white" }}>
             {JSON.stringify(data, null, 2)}
           </pre>
         )}
-        <input type="submit" />
+        <input data-testid="Submit" type="submit" />
       </form>
     </div>
   );
 };
-
 export default ContactForm;
